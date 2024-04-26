f149dc0ee8ebfb8a24d10845ce0cf6f8dd55f3b1
Soloumba <olumbasolomon08@gmail.com>
2024-04-26 14:56:38 +0100

    Plotting throughput

--- appcopy.py
+++ appcopy.py
@@ -20,17 +20,11 @@ collection = db['metrics']  # your collection name
 def fetch_data():
     # Fetch data for node 0 and node 5
     filter_query = {'node': {'$in': [0, 5]}}
-    data = pd.DataFrame(list(collection.find(filter_query, {'_id': 0, 'node': 1, 'time': 1, 'latency': 1})))
+    data = pd.DataFrame(list(collection.find(filter_query, {'_id': 0, 'node': 1, 'time': 1, 'latency': 1, 'throughput': 1})))
     print(data.head())  # Print the first few rows to inspect the DataFrame structure
     return data
 
-# def create_plot(data):
-#     plot = figure(title="Test Plot")
-#     plot.circle([1, 2, 3], [4, 5, 6], size=20)
-#     script, div = components(plot)
-#     return script, div
-
-def create_plot(data):
+def plot_latency (data):
     # Filter data for node 0 and node 5
     data_node0 = data[data['node'] == 0]
     data_node5 = data[data['node'] == 5]
@@ -50,11 +44,32 @@ def create_plot(data):
     return script, div
 
 
+def plot_throughput (data):
+    # Filter data for node 0 and node 5
+    data_node0 = data[data['node'] == 0]
+    data_node5 = data[data['node'] == 5]
+    
+    # Create Bokeh plot
+    plot = figure(title="Throughput over Time by Node", x_axis_label='Time (s)', y_axis_label='Throughput (mbps)', sizing_mode="scale_width")
+    source0 = ColumnDataSource(data_node0)
+    source5 = ColumnDataSource(data_node5)
+    
+    plot.line('time', 'throughput', source=source0, line_width=2, color='green', legend_label='Node 0')
+    plot.line('time', 'throughput', source=source5, line_width=2, color='yellow', legend_label='Node 5')
+    
+    plot.legend.title = 'Node'
+    plot.legend.location = 'top_left'
+    
+    script, div = components(plot)
+    return script, div
+
+
 @app.route("/")
 def index():
     data = fetch_data()
-    script, div = create_plot(data)
-    return render_template('indexcopy.html',  script=script, div=div)
+    latency_script, latency_div = plot_latency (data)
+    throughput_script, throughput_div = plot_throughput (data)
+    return render_template('indexcopy.html',  latency_script=latency_script, latency_div=latency_div, throughput_script=throughput_script, throughput_div=throughput_div)
 
 @app.route("/graph")
 def get_graph():
