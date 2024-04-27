b4c797becf95316ab5ba52b17252b2e930facd94
Soloumba <olumbasolomon08@gmail.com>
2024-04-27 01:31:55 +0100

    Adding a search bar for specific nodes

--- appcopy.py
+++ appcopy.py
@@ -20,7 +20,7 @@ collection = db['metrics']  # your collection name
 def fetch_data():
     # Fetch data for node 0 and node 5
     filter_query = {'node': {'$in': [0, 5]}}
-    data = pd.DataFrame(list(collection.find(filter_query, {'_id': 0, 'node': 1, 'time': 1, 'latency': 1, 'throughput': 1})))
+    data = pd.DataFrame(list(collection.find(filter_query, {'_id': 0, 'node': 1, 'time': 1, 'latency': 1, 'throughput': 1, 'battery': 1})))
     print(data.head())  # Print the first few rows to inspect the DataFrame structure
     return data
 
@@ -63,13 +63,33 @@ def plot_throughput (data):
     script, div = components(plot)
     return script, div
 
+def plot_battery (data):
+    # Filter data for node 0 and node 5
+    data_node0 = data[data['node'] == 0]
+    data_node5 = data[data['node'] == 5]
+    
+    # Create Bokeh plot
+    plot = figure(title="Battery over Time by Node", x_axis_label='Time (s)', y_axis_label='Battery (%)', sizing_mode="scale_width")
+    source0 = ColumnDataSource(data_node0)
+    source5 = ColumnDataSource(data_node5)
+    
+    plot.line('time', 'battery', source=source0, line_width=2, color='black', legend_label='Node 0')
+    plot.line('time', 'battery', source=source5, line_width=2, color='red', legend_label='Node 5')
+    
+    plot.legend.title = 'Node'
+    plot.legend.location = 'top_left'
+    
+    script, div = components(plot)
+    return script, div
+
 
 @app.route("/")
 def index():
     data = fetch_data()
     latency_script, latency_div = plot_latency (data)
     throughput_script, throughput_div = plot_throughput (data)
-    return render_template('indexcopy.html',  latency_script=latency_script, latency_div=latency_div, throughput_script=throughput_script, throughput_div=throughput_div)
+    battery_script, battery_div = plot_battery (data)
+    return render_template('indexcopy.html',  latency_script=latency_script, latency_div=latency_div, throughput_script=throughput_script, throughput_div=throughput_div, battery_script=battery_script, battery_div=battery_div)
 
 @app.route("/graph")
 def get_graph():
